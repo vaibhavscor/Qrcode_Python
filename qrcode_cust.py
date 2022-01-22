58bf23768003afd4adbbad079d7d01be808d4197
Vaibhav Agrawal <50985157+vaibhavscor@users.noreply.github.com>
2022-01-22 11:20:29 +0530

    Add: Add comments

--- qrcode_cust.py
+++ qrcode_cust.py
@@ -1,12 +1,14 @@
-import qrcode
+# Generate QR code for your Url and save your QR to local as png file.
+
+import qrcode           # module for qr generation 
 
 qr = qrcode.QRCode(
     version=1,
     box_size=10,
     border=5
 )
-data = "google.com"
+data = "google.com"                  # url of website whose qr you want
 qr.add_data(data)
 qr.make(fit=True)
 img = qr.make_image(fill="black", back_color="white")
-img.save("1.png")
+img.save("imageQR.png")
